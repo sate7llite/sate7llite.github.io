aeedc7e41debf9a8168f686577d972298e046a57
sate7llite <sate7llite@protonmail.com>
2022-07-25 02:15:42 +0800

    sate7llite: ren [add] assets<dir>

--- assets/js/curveList.js
+++ assets/js/curveList.js
@@ -13,7 +13,7 @@ class TCurveItem
 		var frame = document.createElement('iframe');
 		
 		frame.id           = inx;
-		frame.src          = "js/bareCurve.html";
+		frame.src          = "assets/js/bareCurve.html";
 		frame.width        = "100%";
 		frame.height       = height;
 		frame.scrolling    = "no";
