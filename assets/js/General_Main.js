0af5abacf90a898c235f827a624b02b2f68f89a3
sate7llite <sate7llite@protonmail.com>
2022-07-29 17:04:48 +0800

    <merge> other 3 manmain

--- assets/js/General_Main.js
+++ assets/js/General_Main.js
@@ -281,10 +281,40 @@ class TMemMan extends TFieldDataMan
 //------------------------------------------------------------------
 // [20220729, Whichway] 
 // 3 functions 
+// schmanmain.js
 //------------------------------------------------------------------
 //------------------------------------------------------------------
 
 
+//------------------------------------------------------------------
+// This function will be executed in bareCurve.html!
+//------------------------------------------------------------------
+function UpdateSchdMsg(curveData,curveMsg2,curveMsg3,curveMsg1)
+{
+	curveMsg1.innerHTML = FormatSize(curveData[0].GetLast());
+}
+//------------------------------------------------------------------
+var schdTmpl = ["assets/html/largeCurve.html","0","0","0","0",'#ff0000','#008000',UpdateSchdMsg,[],-1,-1,null];
+//------------------------------------------------------------------
+var schdMeta = 
+[
+	{type:ftDifferentail, field:"taskAdded",   name:"提交"},
+	{type:ftDifferentail, field:"taskSuccess", name:"成功"},
+	{type:ftDifferentail, field:"taskFail",    name:"失败"},
+	{type:ftRaw,          field:"taskReady",   name:"就绪"},
+	{type:ftRaw,          field:"taskBlock",   name:"阻塞"},
+	{type:ftRaw,          field:"taskRun",     name:"运行"},
+
+];
+//------------------------------------------------------------------
+class TSchdMan extends TFieldDataMan
+{
+	constructor(maxDataPoint)
+	{
+		super(maxDataPoint,schdMeta,UpdateSchdMsg,schdTmpl);
+	}
+}
+//------------------------------------------------------------------
 
 
 
@@ -298,6 +328,157 @@ class TMemMan extends TFieldDataMan
 //------------------------------------------------------------------
 // [20220729, Whichway] 
 // 3 functions 
+// taskmanmain.js
 //------------------------------------------------------------------
 //------------------------------------------------------------------
 
+//------------------------------------------------------------------
+class TTaskData extends TCurveDataItem
+{
+	constructor(maxDataPoint)
+	{
+		super(maxDataPoint);
+	}
+	
+	Update(data)
+	{
+		if(this.Cur!=null)
+		{
+			var rt = (data.ulRunTimeCounter-this.Cur.ulRunTimeCounter)/1000000;
+			if(rt<0)
+				rt = 0;
+			else if(rt>1)
+				rt = 1;
+				
+			this.curveData[0].AddData(rt);
+			
+			this.curveData[1].AddData(data.uxContextSwitch-this.Cur.uxContextSwitch);
+			this.curveData[2].AddData(data.uxCurMemAlloc);
+
+			this.curveData[1].CountMax();
+			this.curveData[2].CountMax();
+		}
+		
+		this.Cur = data;
+	}
+}
+//------------------------------------------------------------------
+// This function will be executed in bareCurve.html!
+//------------------------------------------------------------------
+function UpdateTaskMsg(curveData,curveMsg2,curveMsg3,curveMsg1)
+{
+	curveMsg1.innerHTML = (curveData[0].GetLast()*100.0).toFixed(1)+"%";
+	curveMsg2.innerHTML = FormatSize(curveData[1].GetLast());
+	curveMsg3.innerHTML = FormatSize(curveData[2].GetLast())+"B";
+}
+//------------------------------------------------------------------
+const taskTmpl = ["assets/html/largeCurve.html","0","0%","0B","0B",'#ff0000','#008000',UpdateTaskMsg,[],-1,-1,null];
+//------------------------------------------------------------------
+class TTaskMan extends TCurveDataMan
+{
+	constructor(maxDataPoint,maxTask)
+	{
+		super(maxDataPoint,maxTask,TTaskData,UpdateTaskMsg,taskTmpl);
+	}
+
+	UpdateTasks(taskInfo)
+	{
+		this.curData = taskInfo;
+		
+		for(var i=0;i<taskInfo.length;i++)
+		{
+			var inx = taskInfo[i].xTCBInx;
+			
+			this.myData[inx].Update(taskInfo[i]);
+			
+			if(this.myData[inx].inList<0)
+			{
+				var tName = taskInfo[i].pcTaskName;
+				
+				if(tName.length>6)
+					tName = tName.substr(0,6);
+				
+				this.myCurve[inx][clMainTitle] = tName;
+
+				this.activeCurve.push(this.myCurve[inx]);
+				this.myData[inx].inList = inx;
+			}
+		}
+	}
+}
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+// [20220729, Whichway] 
+// 3 functions 
+// taskmanmain.js
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+class TTimerData extends TCurveDataItem
+{
+	constructor(maxDataPoint)
+	{
+		super(maxDataPoint);
+		this.curveData.length = 1;
+	}
+	
+	Update(data)
+	{
+		if(this.Cur!=null)
+		{
+			this.curveData[0].AddData(data.nTrigger-this.Cur.nTrigger);
+			this.curveData[0].CountMax();
+		}
+		
+		this.Cur = data;
+	}
+}
+//------------------------------------------------------------------
+// This function will be executed in bareCurve.html!
+//------------------------------------------------------------------
+function UpdateTimerMsg(curveData,curveMsg2,curveMsg3,curveMsg1)
+{
+	curveMsg1.innerHTML = FormatSize(curveData[0].GetLast());
+}
+//------------------------------------------------------------------
+const timerTmpl = ["../html/largeCurve.html","0","0","0","0",'#ff0000','#008000',UpdateTimerMsg,[],-1,-1,null];
+const timerNames = 
+[
+	"用户0", "用户1", "用户2", "用户3",	"时钟4",
+	"唤醒0", "唤醒1", "唤醒2", "唤醒3", "唤醒4", "唤醒5", "唤醒6", "唤醒7" 
+];
+//------------------------------------------------------------------
+class TTimerMan extends TCurveDataMan
+{
+	constructor(maxDataPoint,maxTimer)
+	{
+		super(maxDataPoint,maxTimer,TTimerData,UpdateTimerMsg,timerTmpl);
+		
+		for(var i=0;i<maxTimer;i++)
+		{
+			this.myCurve[i][clMainTitle] = timerNames[i];
+		}
+	}
+
+	UpdateTimers(timerInfo)
+	{
+		this.curData = timerInfo;
+		
+		for(var i=0;i<timerInfo.length;i++)
+		{
+			var inx = timerInfo[i].id;
+			
+			this.myData[inx].Update(timerInfo[i]);
+			
+			if(this.myData[inx].inList<0)
+			{
+				this.activeCurve.push(this.myCurve[inx]);
+				this.myData[inx].inList = inx;
+			}
+		}
+	}
+}
+//------------------------------------------------------------------
