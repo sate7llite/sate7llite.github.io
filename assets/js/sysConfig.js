6514756751e6e315f2913e263ef95c2a9a766bd0
sate7llite <sate7llite@protonmail.com>
2022-07-29 17:49:59 +0800

    <curve> id = 100

--- assets/js/sysConfig.js
+++ assets/js/sysConfig.js
@@ -13,8 +13,8 @@ var jsSvrPort      = 1375;
 var jsDataDlPort   = 1471;
 var jsResultPort   = 1472;
 var jsSvrIP        = window.location.hostname;
-var jsURL          = window.location.protocol+"//"+jsSvrIP+":"+jsSvrPort+"/assets/js/current.json";
-var jsSchdStatURL  = window.location.protocol+"//"+jsSvrIP+":"+jsSvrPort+"/assets/js/currentSchd.json";
-var jsSchdDataURL  = window.location.protocol+"//"+jsSvrIP+":"+jsSvrPort+"/assets/js/currentTasks.json";
+var jsURL          = window.location.protocol+"//"+jsSvrIP+":"+jsSvrPort+"/../assets/js/current.json";
+var jsSchdStatURL  = window.location.protocol+"//"+jsSvrIP+":"+jsSvrPort+"/../assets/js/currentSchd.json";
+var jsSchdDataURL  = window.location.protocol+"//"+jsSvrIP+":"+jsSvrPort+"/../assets/js/currentTasks.json";
 var jsDataListURL  = "/assets/js/dataList.json";     
 //------------------------------------------------------------------
